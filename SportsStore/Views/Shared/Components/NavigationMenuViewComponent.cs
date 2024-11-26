d9bbcf51d2be668f214dfe4f0bba4a0ebf12aaf7
ANDERSON SILVA <alucardand2017@gmail.com>
2024-11-26 17:46:04 -0300

    Creating the View and  Highlighting the Current Category

---
+++ SportsStore/Views/Shared/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+public class NavigationMenuViewComponent : ViewComponent {
+    private IStoreRepository  repository;
+    public NavigationMenuViewComponent(IStoreRepository repo) {
+        repository = repo;
+    }
+    public IViewComponentResult Invoke() {
+         ViewBag.SelectedCategory = RouteData?.Values["category"];
+        return View(repository.Products
+        .Select(x => x.Category)
+        .Distinct()
+        .OrderBy(x => x));
+    }
+}
